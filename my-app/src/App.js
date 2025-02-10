92771f62b549485477f73535df4f76a29de752b3
James-Carnathan <Jamescarnathan06@gmail.com>
2025-02-10 13:14:50 -0500

    Game set

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import Phaser from 'phaser';
 
 function App() {
+  useEffect(() => {
+    const game = new Phaser.Game({
+      type: Phaser.AUTO,
+      width: 980,
+      height: 720,
+      backgroundColor: '#F2F0EF',
+      parent: 'gameCanvas',
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 0 },
+          debug: false,
+        },
+      },
+      scene: {
+        preload: function () {},
+        create: function () {
+          this.player = this.add.circle(400, 300, 30, 0x00ff00);
+          this.physics.world.enable(this.player);
+          this.player.body.setCollideWorldBounds(true);
+          this.cursors = this.input.keyboard.createCursorKeys();
+        },
+        update: function () {
+          if (this.cursors.left.isDown) {
+            this.player.x -= 5;
+          } else if (this.cursors.right.isDown) {
+            this.player.x += 5;
+          }
+          if (this.cursors.up.isDown) {
+            this.player.y -= 5;
+          } else if (this.cursors.down.isDown) {
+            this.player.y += 5;
+          }
+          this.player.x = Phaser.Math.Clamp(this.player.x, 30, 950);
+          this.player.y = Phaser.Math.Clamp(this.player.y, 30, 690);
+        }
+      }
+    });
+
+    return () => {
+      game.destroy(true);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Circle Game</h1>
+      <div id="gameCanvas" />
     </div>
   );
 }
